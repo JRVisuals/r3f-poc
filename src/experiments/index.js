6605846aa39075940c901e35e8925d64d80cfa33
Jose Rodriguez <jrod.disco@gmail.com>
2019-11-07 16:07:11 -0500

    exp-turrel // fiddling w/ the cubes (lights and shadow)

--- src/experiments/index.js
+++ src/experiments/index.js
@@ -2,12 +2,14 @@
 import React from 'react'
 import SimpleCube from './simple_cube/'
 import CubeTwo from './cube_plus/'
+import TurrelCube from './cube_turrel/'
 
 
-export { SimpleCube, CubeTwo };
+export { SimpleCube, CubeTwo, TurrelCube };
 
 // Object to be used for route resolution
 export const ExperimentList = {
     cube: <SimpleCube/>,
     cube_two: <CubeTwo/>,
+    turrel_cube: <TurrelCube/>,
 }

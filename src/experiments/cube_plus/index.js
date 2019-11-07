9d61f38346ea6ebede85cf930831cd21188de1f8
Jose Rodriguez <jrod.disco@gmail.com>
2019-11-07 13:18:06 -0500

    exp-cube2 // fiddling w/ the cubes (lights)

--- src/experiments/cube_plus/index.js
+++ src/experiments/cube_plus/index.js
@@ -18,22 +18,36 @@ const Thing = () => {
         <group>
 
             <spotLight
-                intensity={0.6}
-                position={[0, 0, 25]}
-                angle={0.3}
-                penumbra={1}
+                color="#FFF"
+                intensity={0.75}
+                position={[-10, 35, 10]}
+                angle={0.53}
+                penumbra={0.75}
                 shadow-mapSize-width={2048}
                 shadow-mapSize-height={2048}
                 castShadow
             />
+
+            <spotLight
+            color="red"
+            intensity={0.25}
+            position={[0, 0, 50]}
+            angle={0.2}
+            penumbra={0.75}
+            shadow-mapSize-width={2048}
+            shadow-mapSize-height={2048}
+            castShadow
+        />
+
             <group ref={cubes}>
                 <mesh
                     ref={cubeZero}
                     onClick={e => console.log('click')}
                     onPointerOver={e => console.log('hover')}
                     onPointerOut={e => console.log('unhover')}>
-                    <boxBufferGeometry attach="geometry" args={[1.0, 1.0, 1.0]} />
+                    <boxBufferGeometry attach="geometry" args={[1.0, 1.0, 1.0]}/>
                     <meshPhongMaterial attach="material" color="#FF7744"/>
+
                 </mesh>
                 <mesh
                     ref={cubeOne}
@@ -42,6 +56,7 @@ const Thing = () => {
                     onPointerOut={e => console.log('unhover')}>
                     <boxBufferGeometry attach="geometry" args={[2.5, 2.5, 2.5]} />
                     <meshPhongMaterial attach="material" color="#AAFFDD" opacity={0.5} transparent/>
+
                 </mesh>
                 <mesh
                     ref={cubeTwo}
@@ -50,8 +65,13 @@ const Thing = () => {
                     onPointerOut={e => console.log('unhover')}>
                     <boxBufferGeometry attach="geometry" args={[5, 5, 5]} />
                     <meshPhongMaterial attach="material" color="#AABBFF" opacity={0.25} transparent/>
+
                 </mesh>
             </group>
+            <mesh receiveShadow position={[0,0,-10]}>
+                <planeBufferGeometry attach="geometry" args={[1000, 1000]} />
+                <meshPhongMaterial attach="material" color="#555555" />
+            </mesh>
         </group>
     )
 }

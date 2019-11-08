211c1ad6fb8b98186440f93e7cc9562c56aa5172
jrod-disco <jrod.disco@gmail.com>
2019-11-08 17:26:15 -0500

    metadata-display // move links CSS over to component

--- src/components/ExperimentLinks/index.js
+++ src/components/ExperimentLinks/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import './styles.css'
+
 const ExperimentLinks = ({ experiments }) => {
     return (
         <div className="ExperimentLinks">

004aee5c7aeb9eb3141d40474506ffe9fe117105
jrod-disco <jrod.disco@gmail.com>
2019-11-16 10:30:58 -0500

    shaders // added some shaders and exports of examples

--- src/shaders/index.js
+++ src/shaders/index.js
@@ -1,4 +1,14 @@
+// Shaders Plucked off the web
 import AdditiveBlendingShader from './AdditiveBlendingShader';
 import VolumetricLightShader from './VolumetricLightShader';
+import Glitchpass from './Glitchpass';
+import Waterpass from './Waterpass';
 
-export { AdditiveBlendingShader, VolumetricLightShader };
+export { AdditiveBlendingShader, VolumetricLightShader, Glitchpass, Waterpass };
+
+// Effect Composer and Example Shaders
+export { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
+export { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
+export { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
+export { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass';
+export { FilmPass } from 'three/examples/jsm/postprocessing/FilmPass';

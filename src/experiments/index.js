37b859e878bf7f4ccc0af4efd59d66e6ad864a6e
jrod-disco <jrod.disco@gmail.com>
2019-11-18 02:08:34 -0500

    loaders // new non-suspense loader, renamed experiments to suit

--- src/experiments/index.js
+++ src/experiments/index.js
@@ -3,6 +3,7 @@ import simpleCube from './simple_cube/';
 import anotherCube from './cube_plus/';
 import turrellCube from './cube_turrell/';
 import modelStatic from './model_static/';
+import modelSuspense from './model_suspense/';
 import modelAnimated from './model_animated/';
 
 // Object to be used for route resolution
@@ -11,6 +12,7 @@ const experiments = [
   anotherCube,
   turrellCube,
   modelStatic,
+  modelSuspense,
   modelAnimated,
 ];
 
--- src/experiments/model_static/index.js
+++ src/experiments/model_static/index.js
@@ -1,43 +1,39 @@
-import React, { Suspense, useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
-import { useLoader, useFrame } from 'react-three-fiber';
+import { useFrame } from 'react-three-fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 import model_static from '../../models/mario.glb';
 
 const Model = ({ fileRef }) => {
-  const modelRef = useRef();
+  const [modelRender, setModelRender] = useState(null);
 
-  const gltf = useLoader(GLTFLoader, fileRef);
+  useEffect(() => {
+    const loader = new GLTFLoader();
+    try {
+      loader.load(fileRef, gltf => {
+        const modelPos = [0, 0, 0];
+        const modelScale = [0.05, 0.05, 0.05];
+        setModelRender(
+          <group position={modelPos} scale={modelScale}>
+            <primitive object={gltf.scene} />
+          </group>
+        );
+      });
+    } catch (e) {
+      console.log(`GLTFLoader error ${e}`);
+    }
+  }, [fileRef]);
 
-  useFrame(() => {
-    modelRef.current.rotation.y += 0.02;
-  });
-
-  // Mario model is huuuge
-  const modelPos = [0, 20, -100];
-  const modelScale = [0.05, 0.05, 0.05];
-
-  return (
-    <group ref={modelRef} position={modelPos} scale={modelScale}>
-      <primitive object={gltf.scene} />
-    </group>
-  );
-};
-
-// Basic spinning cube loader to display in Suspense
-const Loader = () => {
-  return (
-    <mesh>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial attach="material" transparent opacity={0.5} />
-    </mesh>
-  );
+  return <React.Fragment>{modelRender}</React.Fragment>;
 };
 
 export const ModelStatic = () => {
   const modelFile = model_static;
-
+  const modelRef = useRef();
+  useFrame(() => {
+    modelRef.current.rotation.y += 0.02;
+  });
   return (
     <group>
       <ambientLight color="#FFF" intensity={0.5} />
@@ -53,9 +49,9 @@ export const ModelStatic = () => {
         receiveShadow
       />
 
-      <Suspense fallback={<Loader />}>
+      <group ref={modelRef} position={[0, 20, -100]}>
         <Model fileRef={modelFile} />
-      </Suspense>
+      </group>
     </group>
   );
 };
@@ -64,9 +60,22 @@ export default {
   id: 'model_static',
   component: ModelStatic,
   metadata: {
-    name: 'Model(STATIC)',
+    name: 'Model Static',
     author: '',
     description:
-      'Loads static model of Mario using React.Suspense and external .glb files. Model credit: Captain LowPoly on sketchfab',
+      'Loads static model of Mario from an external .glb file w/o using React.Suspense. Model credit: Captain LowPoly on sketchfab',
   },
 };
+
+/* Loader Notes
+
+  Rather than using the useLoader hook and React.suspense we load the model directly with the GLTF loader.
+  The loader is called in a useEffect hook based on the fileRef (which should never change, if it did change it should automatically reload and update).
+  The returned component is initially an empty fragment but updated via a useState hook once the loader is complete.
+  Key to note that R3F is fragment friendly.
+
+  Initially I tried doing all of this with async/await and got into a bind with components as promises. There may have been a way around that but this feels cleaner for a single model.
+
+  Bonus: Removing suspense fixes are repeat load missing model issue.
+  
+*/
